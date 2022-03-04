3a1b89cde220c19bad25c13bf54a011aec49076e
mrAhmad <naashna20@gmail.com>
2022-03-04 22:28:47 +0330

    modify toggle

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ sideIconMenus.forEach(icon=>{
   icon.addEventListener('click',e => {
 
     //get clicked icon side dynamically and add active class to it's side menu
-    let clickedIcon = e.target.closest('.top-navigation-menus span').parentElement.className.includes('left') ? 'left' : 'right';
+    let clickedIcon = e.currentTarget.parentElement.className.includes('left') ? 'left' : 'right';
     document.querySelector(`.${clickedIcon}-menu`).classList.toggle('active')
   })
 })
