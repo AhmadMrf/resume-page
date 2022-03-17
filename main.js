a27202e5978cdae3cf052b5a7a6c3c9fb0fdd60d
ahmad-mrf <naashna20@gmail.com>
2022-03-17 11:37:32 +0330

    modify toggle navbar icon

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 // toggle side menus
 
-const sideIconMenus = document.querySelectorAll('.top-navigation-menus span');
+const topNavigation = document.querySelector('.top-navigation-menus')
+const sideIconMenus = document.querySelectorAll('.top-navigation-menus span')
+const leftMenu = document.querySelector('.left-menu')
+const rightMenu = document.querySelector('.right-menu')
+
 
 sideIconMenus.forEach(icon=>{
   icon.addEventListener('click',e => {
 
-    //get clicked icon side dynamically and add active class to it's side menu
     let clickedIcon = e.currentTarget.parentElement.className.includes('left') ? 'left' : 'right';
     document.querySelector(`.${clickedIcon}-menu`).classList.toggle('active')
+
+    if(clickedIcon === 'left'){
+      icon.classList.toggle('active')
+    }
   })
+})
+
+
+addEventListener('click' , e => {
+  if(e.target.closest('.top-navigation-menus') !== topNavigation){
+    if(e.target.closest('.left-menu') !== leftMenu){
+      leftMenu.classList.remove('active')
+      sideIconMenus[0].classList.remove('active') // left span
+    }
+    if(e.target.closest('.right-menu') !== rightMenu){
+    rightMenu.classList.remove('active')
+    }
+  }
 })
