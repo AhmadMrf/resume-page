4d948f7bc12f5d337347bbf5e4325233d6858042
Ahmad_Mrf <44136738+AhmadMrf@users.noreply.github.com>
2022-10-19 11:30:38 +0330

    dynamicly added comments to page

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+import { fetchDataFromDummyjsonApi } from "./requests.js";
 const topNavigation = document.querySelector(".top-navigation-menus");
 const sideIconMenus = document.querySelectorAll(".top-navigation-menus span");
 const leftMenu = document.querySelector(".left-menu");
@@ -16,18 +17,84 @@ const portfolioBox = document.querySelector("#portfolio-boxes");
 const loadMore = document.querySelector("#morePortfolio");
 const toggleThemeIcon = rightMenu.querySelector(".toggle-theme");
 const toggleThemeTitle = toggleThemeIcon.querySelector("span");
+const commentWrapper = document.querySelector("#recommendations .main-menu-content-wrapper");
+// ## get data from API
+
+const postsApi = "https://dummyjson.com/posts";
+let commentCount = 3;
+// ## create posts and comments dynamicly
+commentWrapper.innerHTML = Array.from({ length: commentCount }, () => {
+  return `
+  <div class="recommend-box pre-loader">
+    <div class="recommend-rating-stars">
+      <span>** **</span>
+    </div>
+    <h4>comment title</h4>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna viverra
+    morbi. Morbi donec amet....
+    </p>
+    <div class="recommend-box-profile">
+    <span class="image"></span>
+    <div>
+    <h4>fullname user</h4>
+    <p>user job</p>
+    </div>
+    </div>
+    </div>
+    `;
+}).join("");
+
+// const { result: posts, error } = await fetchDataFromDummyjsonApi(postsApi, commentCount, true); //fetchDataFromApi(endpoint [ url ],limit count [ number ],get post users [true])
+// const d = await fetchDataFromDummyjsonApi(postsApi, commentCount, true); //fetchDataFromApi(endpoint [ url ],limit count [ number ],get post users [true])
+// console.log(d);
+fetchDataFromDummyjsonApi(postsApi, commentCount, true).then(({ result: posts, error }) => {
+  if (posts) {
+    let comments = posts.map((post) => {
+      let stars = Array.from({ length: post.reactions }, (star) => {
+        return `<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M9 13.7115L12.9107 12.722L14.5446 17.75L9 13.7115ZM18 7.2125H11.1161L9 0.740234L6.88393 7.2125H0L5.57143 11.2242L3.45535 17.6965L9.02679 13.6848L12.4554 11.2242L18 7.2125Z" fill="#FFB400"></path>
+      </svg>`;
+      }).join("");
+
+      return `
+      <div class="recommend-box">
+          <div class="recommend-rating-stars">
+          ${stars}
+          </div>
+          <h4>${post.title}</h4>
+          <p>${post.body}</p>
+          <div class="recommend-box-profile">
+            <img src="${post.userId.image}" alt="profile 1 image">
+            <div>
+              <h4>${post.userId.firstName} ${post.userId.lastName}</h4>
+              <p>${post.userId.company.name}</p>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+    commentWrapper.innerHTML = comments.join("");
+  } else {
+    console.log("not loaded", error);
+  }
+});
 
 //## toggle dark mode ##
-
 let isDark;
-addEventListener("load", () => {
+window.addEventListener("load", () => {
   // check localstorage for existing
   if (!localStorage.getItem("isDark")) {
     localStorage.setItem("isDark", JSON.stringify(false));
   } else {
     toggleTheme(1);
   }
+  let sectionInViewId = getInViewSection();
+  highlight(sectionInViewId);
+  console.log(55);
+  createDotSliders(document.querySelectorAll(".one-row-boxes"));
 });
+
 toggleThemeIcon.addEventListener("click", () => {
   toggleTheme(0);
 });
@@ -67,7 +134,7 @@ sideIconMenus.forEach((icon) => {
 
 // when click outside menus and top navigation , close menus
 
-addEventListener("click", (e) => {
+window.addEventListener("click", (e) => {
   if (e.target.closest(".top-navigation-menus") !== topNavigation) {
     if (e.target.closest(".left-menu") !== leftMenu) {
       leftMenu.classList.remove("active");
@@ -153,7 +220,7 @@ portfolioDialog.addEventListener("click", (e) => {
 // get element atribute for dialog
 let src, alt;
 
-portfolioContent = function (box) {
+const portfolioContent = function (box) {
   src = box.firstElementChild.firstElementChild.getAttribute("src");
   alt = box.firstElementChild.firstElementChild.getAttribute("alt");
   portfolioDialogContent.setAttribute("src", src);
@@ -227,16 +294,12 @@ document.body.addEventListener("scroll", () => {
   highlight(sectionInViewId);
 });
 
-window.addEventListener("load", () => {
-  let sectionInViewId = getInViewSection();
-  highlight(sectionInViewId);
-});
-
 let arraySections = Array.from(sections);
 function getInViewSection() {
   let sectionInView = arraySections.findIndex((section) => {
     return section.getBoundingClientRect().y - 50 > 1;
   });
+  let sectionInViewId;
   return (sectionInViewId = sections[sectionInView - 1].id);
 }
 
@@ -259,32 +322,36 @@ goToTopBtn.addEventListener("click", () => {
 });
 
 //  ## slider
+let debounce;
 window.addEventListener("resize", () => {
-  createDotSliders(document.querySelectorAll(".one-row-boxes"));
+  clearTimeout(debounce);
+  debounce = setTimeout(() => {
+    console.log(44);
+    createDotSliders(document.querySelectorAll(".one-row-boxes"));
+  }, 200);
 });
-createDotSliders(document.querySelectorAll(".one-row-boxes"));
-
 function sliderHandler(dot) {
   let WhichDot = dot.dataset.index;
-  console.log(WhichDot);
+  // console.log(WhichDot);
   let sliderContainer = dot.parentElement.previousElementSibling;
   let totalWidth = sliderContainer.scrollWidth;
   let currentView = sliderContainer.clientWidth;
   sliderContainer.scrollLeft = 225 * WhichDot;
-
 }
 
 function createDotSliders(sliderContainer) {
+  // console.log(sliderContainer);
+  console.log(sliderContainer);
+
   sliderContainer.forEach((slider) => {
     slider.nextElementSibling.innerHTML = "";
     let eachSlideWidth = slider.scrollWidth / 3;
     let viewWidth = slider.offsetWidth;
-    let countSlideInView = (viewWidth / eachSlideWidth);
-    let dotCount = countSlideInView <= 1.7 ? 3 : countSlideInView ===3 ? 0 : 2;
+    let countSlideInView = viewWidth / eachSlideWidth;
+    let dotCount = countSlideInView <= 1.7 ? 3 : countSlideInView === 3 ? 0 : 2;
     let dots = [];
-    for (i = 0; i < dotCount; i++) {
+    for (let i = 0; i < dotCount; i++) {
       let dot = document.createElement("span");
-      dot.setAttribute("onclick", "sliderHandler(this)");
       dot.setAttribute("data-index", i);
       dots = [...dots, dot];
     }
@@ -293,3 +360,6 @@ function createDotSliders(sliderContainer) {
     slider.scrollLeft = 0;
   });
 }
+window.addEventListener("readystatechange", (e) => {
+  console.log(document.readyState);
+});
